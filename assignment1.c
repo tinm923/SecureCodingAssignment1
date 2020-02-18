b9b1fb386615179d977acc653de6eed2bb85436c
tinm923 <54872572+tinm923@users.noreply.github.com>
2020-02-18 16:31:27 -0500

    change made to read up to 10 lines

--- assignment1.c
+++ assignment1.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+#define MAXCHAR 512
+#define MAXLINES 10
+void displayContents();
 int main() {
     FILE *testFile;
     testFile = fopen("a.txt", "r");
+    if (testFile == NULL){
+        perror("Error opening the file");
+        return 1;
+    }
+    char **buff = (char**)malloc(1 * sizeof(char*));
+    for (int i = 0; i < MAXLINES; i++){
+	    buff = (char**)realloc(buff, (i + 1) * sizeof(char*));
+	    buff[i] = malloc(MAXCHAR);
+		fgets(buff[i], MAXCHAR, testFile);
+	}
 
-    char buff[200];
-    fgets(buff, 200, (FILE*)testFile);
-    printf("Content of first line: %s", buff);
-    
+    displayContents(buff);
+    free(buff);
     fclose(testFile);
     return 0;
+}
+
+void displayContents(char **input){
+    for (int i = 0; i < MAXLINES; i++)
+	{
+		printf("O/P: %s\n", input[i]);
+		free(input[i]);
+	}
 }
